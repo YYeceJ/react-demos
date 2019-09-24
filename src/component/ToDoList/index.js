f0599e0f53b6f3df9962c84a5eef4272554817d1
jiangyy <yyecej@163.com>
2019-09-24 12:01:26 +0800

    完成功能

--- src/component/ToDoList/index.js
+++ src/component/ToDoList/index.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
 import './style.css'
-import { Button,Input,Collapse } from 'element-react'
+import {Button, Input, Collapse, Checkbox} from 'element-react'
 import 'element-theme-default';
 
 let data = [
     {
-        "content" : "吃早饭",
-        "done" : true
+        "content": "吃早饭",
+        "done": true
     },
     {
-        "content" : "吃午饭",
-        "done" : true
+        "content": "吃午饭",
+        "done": true
     },
     {
-        "content" : "吃晚饭",
-        "done" : false
+        "content": "吃晚饭",
+        "done": false
     }
 ]
 
@@ -24,7 +24,8 @@ class ToDoList extends Component {
         super(props, context);
 
         this.state = {
-            todoList : data,
+            todoList: data,
+            text: ''
         }
     }
 
@@ -37,56 +38,99 @@ class ToDoList extends Component {
         );
     }
 
-    renderInput () {
+    renderInput() {
         return (
-             <form onSubmit={event => event.preventDefault()} className='inputContainer'>
-                 <Input type='text' name='text' placeholder='请输入要做的事' ref='todoInput' autoFocus={true} />
-                 <Button type='primary' onClick={e => this.handleClickButton()}>提交</Button>
+            <form onSubmit={event => event.preventDefault()} className='inputContainer'>
+                <input type='text' name='text'
+                       placeholder='请输入要做的事'
+                       ref='todoInput'
+                       autoFocus={true}
+                       className='todoInput'
+                />
+                <button onClick={e => this.handleClickButton()} className='todoButton'>提交</button>
             </form>
         )
     }
 
-    async handleClickButton()   {
-        let text = this.refs.todoInput.value
-        let list = this.state.todoList
-        if(text === ''){
-            alert('请输入要做的事')
-        }else {
-            alert(text)
-            await list.push({"content":text,"done":false})
-            this.setState({
-                todoList : list
-            })
-            this.refs.todoInput.value = ''
-        }
-    }
-
-    renderList () {
-        const activeName = "todo";
-        let  totalArr = this.state.todoList
-        alert(JSON.stringify(totalArr))
-        let  todoArr = totalArr.filter(item => item.done === false)
-        let  doneArr = totalArr.filter(item => item.done === true)
+    renderList() {
+        let totalArr = this.state.todoList
+        let todoArr = totalArr.filter(item => item.done === false)
+        let doneArr = totalArr.filter(item => item.done === true)
         return (
-            <Collapse value={activeName}>
-                {this.renderCollapseItem(todoArr)}
-                {this.renderCollapseItem(doneArr)}
-            </Collapse>
+            <div className='notesContainer'>
+                {this.renderNotesItem(todoArr)}
+                {this.renderNotesItem(doneArr)}
+            </div>
         )
     }
 
-    renderCollapseItem(arr){
-        let isDone = arr[0].done === true
-        if(arr.length > 0 && arr !== null){
-           return (
-               <Collapse.Item title={isDone ? "已完成" : "正在进行"} name={isDone ?  "done" : "todo"} >
-                   {
-                       arr.map(item => {
-                           return  <div>{item.content}</div>
-                       })
-                   }
-               </Collapse.Item>
-           )
+    handleClickButton() {
+        let text = this.refs.todoInput.value
+        let arr = this.state.todoList
+        if (text !== '') {
+            arr.push({"content": text, "done": false})
+            this.setState({
+                todoList: arr
+            })
+            this.refs.todoInput.value = ''
+        } else {
+            alert('请输入内容')
+        }
+    }
+
+    renderNotesItem(arr) {
+        if (arr.length > 0 && arr !== null) {
+            let isDone = arr[0].done === true
+            return (
+                <div className='noteContainer'>
+                    <span>
+                        {
+                            arr[0].done === true ? <div> 已完成 </div> : <div>正在进行</div>
+                        }
+                        {arr.length}
+                    </span>
+                    {
+                        arr.map((item, index) => {
+                            return (
+                                <div>
+                                    <Checkbox checked={isDone} onChange={e => this.handleClickCheckbox(item)}>
+                                        {item.content}
+                                    </Checkbox>
+                                    <i className="el-icon-delete" onClick={e => this.handleClickDeleteButton(item)}></i>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            )
+        }
+    }
+
+    handleClickCheckbox(item) {
+        if (item) {
+            let arr = this.state.todoList
+            arr.forEach(element => {
+                if (element.content == item.content) {
+                    element.done = !item.done
+                }
+            })
+            this.setState({
+                todoList: arr
+            })
+        }
+    }
+
+    handleClickDeleteButton(item) {
+        let arr = this.state.todoList
+        if(item){
+           arr.forEach((element,index) => {
+               if (element.content == item.content) {
+                   arr.splice(index,1)
+               }
+           })
+            this.setState({
+                todoList : arr
+            })
         }
     }
 }
