6f30c2435488475cd277e115a7ea991697e12041
jiangyy <yyecej@163.com>
2019-10-11 17:47:44 +0800

    改了一下TODOList的文件结构

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Calculator from './TemperatureConvert/index'
-import Parent from './parent-child-ref/index'
+// import Parent from './parent-child-ref/index'
+import ToDoList from "./ToDoList";
 ReactDOM.render(
-  <Parent />,
+  <ToDoList />,
   document.getElementById('root')
 );
 
